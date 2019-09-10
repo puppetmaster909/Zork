f315074033acbc7fd6adb517a8715c27c2231d9c
Thomas Pring <pringthomas4@gmail.com>
2019-09-10 18:04:20 -0400

    Added project files and Welcome to Zork
    Added Visual Studio 2019 files to repo and changed print statement to be "Welcome to Zork!"

---
+++ Zork/Zork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zork
+{
+    class Zork
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+        }
+    }
+}
