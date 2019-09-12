56919e358e4e7871ce253b7632c34ae581a3f595
Thomas Pring <pringthomas4@gmail.com>
2019-09-12 18:16:20 -0400

    Finished 3-2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,24 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+
+            switch (inputString)
+            {
+                case "QUIT":
+                    Console.WriteLine("Thank you for playing.");
+                    break;
+
+                case "LOOK":
+                    Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!'  lies by the door");
+                    break;
+
+                default:
+                    Console.WriteLine("Unrecognized command");
+                    break;
+            }
         }
     }
 }
